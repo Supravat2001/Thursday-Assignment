a8dffdde4bdde29b2f8a6cad91dc7fcc4c2d9469
Supravat2001 <106583692+Supravat2001@users.noreply.github.com>
2022-07-04 09:55:37 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,36 @@
+package arrayexamples;
+
+import java.util.Scanner;
+
+public class ReverseArray
+{
+
+	public static void main(String[] args) 
+	{
+		
+			int j=4;
+			int array[]=new int[5];
+			int array2[]=new int[5];
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter Elements To Reverse: ");
+			for(int i=0;i<5;i++) {
+				
+				array[i]=sc.nextInt();
+			}
+			System.out.println("Reversed elements are: ");
+	for(int i=0;i<array.length;i++) {
+		
+		array2[j]=array[i];
+		j--;
+		
+	}
+	for(int i=0;i<array2.length;i++) {
+		
+		System.out.println("element in index: "+i+" is:"+" "+array2[i]);
+	}
+		}
+	
+
+	}
+
+
